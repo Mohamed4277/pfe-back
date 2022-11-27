9c381e1025152d208081de561741d0972df500d5
Mai <mohamed.aissaoui.perso@gmail.com>
2022-11-27 16:18:59 +0100

    update user date

--- src/main/resources/db/migration/V4__insertUser.sql
+++ src/main/resources/db/migration/V4__insertUser.sql
@@ -1,5 +1,5 @@
 INSERT INTO user (name,password,username)
-values ("mai@mai.fr","$2a$10$lvD4ipG1fR.JCpeFViV3aOXpjupRcGspP1Y2jtjYUjW/8mGKOAhRy","mai@mai.fr");
+values ("mai@mai.fr","$2a$10$C5Z9ftaa5QTLJAW390SGT.wZtV.NO8JnhOyRvYwcqki.rwTWT.tMG","mai@mai.fr");
 
 INSERT INTO user_roles (user_id,roles_id) values
 ((SELECT id FROM user WHERE username="mai@mai.fr"), (SELECT id FROM role where name="ROLE_USER"));
